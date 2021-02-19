1e57badf1fb33860d007877a29f891cc85cffc1a
Alessandro <dixiedream@hotmail.it>
2021-02-19 18:23:38 +0100

    Works

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int gappx     = 3;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -95,6 +95,7 @@ static Key keys[] = {
 	{ MODKEY|ControlMask,           XK_Return, zoom,           {0} },
 	{ MODKEY|ShiftMask,             XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+    { MODKEY,                       XK_f,      togglefullscr,  {0} },
 
 	/* Layout manipulation */
 	{ MODKEY,           			XK_space,  cyclelayout,    {.i = +1 } },
